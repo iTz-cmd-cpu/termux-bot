728bad686af637015877759fc30d51f95a02a729
iTz-cmd-cpu <78374188+iTz-cmd-cpu@users.noreply.github.com>
2021-02-05 15:31:55 -0300

    Update index.js

--- index.js
+++ index.js
@@ -131,7 +131,7 @@ async function starts() {
 			}
 
 			const botNumber = client.user.jid
-			const ownerNumber = ["31983411248@s.whatsapp.net"] // replace this with your number
+			const ownerNumber = ["5531983411248"] // replace this with your number
 			const isGroup = from.endsWith('@g.us')
 			const sender = isGroup ? mek.participant : mek.key.remoteJid
 			const groupMetadata = isGroup ? await client.groupMetadata(from) : ''
